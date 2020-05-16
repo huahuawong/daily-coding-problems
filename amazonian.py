66f6ce4c9640e4e8133fc1130c2f515829539bd8
huahuawong <junhuawong96@gmail.com>
2020-05-16 14:32:29 -0400

    Update amazonian.py

--- amazonian.py
+++ amazonian.py
@@ -1,1 +1,61 @@
+#1 Given a string, determine whether any permutation of it is a palindrome.
+
+For example, carrace should return true, since it can be rearranged to form racecar, which is a palindrome. 
+daily should return false, since there's no rearrangement that can form a palindrome.
+
+NO_OF_CHARS = 256
+
+def canformPalindrone(st):
+    # Create a count array and initialize
+    # all values as 0
+    count = [0] * NO_OF_CHARS
+
+    # For each character in input strings,
+    # increment count in the corresponding
+    # count array
+    for i in range(0, len(st)):
+        count[ord(st[i])] = count[ord(st[i])] + 1
+    # a little confusing but basically we count the characteres to check
+    # if there is more than one odd values, cause if there is, then palindrone can;t be formed
+
+    # Count odd occurring characters
+    odd = 0
+
+    for i in range(0, NO_OF_CHARS):
+        if (count[i] & 1):
+            odd = odd + 1
+
+        if (odd > 1):
+            return False
+
+    # Return true if odd count is 0 or 1,
+    return True
+
+
+str = "mmo"
+print(canformPalindrone(str))
+
+
+############################################################################################################################
+#2 Given a sorted array, find the smallest positive integer that is not the sum of a subset of the array.
+
+def findSmallest(arr, n): 
+  
+    res = 1 #Initialize result, we start from 1 because it cant be represented if it's not in the array, and also smallest one
+  
+    # Traverse the array and increment 
+    # 'res' if arr[i] is smaller than 
+    # or equal to 'res'. 
+    for i in range (0, n ): 
+        if arr[i] <= res: 
+            res = res + arr[i]   #the reason we increment by that is because if arr[i] is smaller or equals to res,
+                                 #means that we can represent the numbers from 0 to i
+        else: 
+            break
+    return res 
+    
+    
+array = [1, 1, 3, 4] 
+n1 = len(array) 
+print(findSmallest(array, n1))
 
