cb9a451d3bff9263a32c2e0b682c7543b2c54583
huahuawong <junhuawong96@gmail.com>
2020-05-20 19:45:03 -0400

    Update and rename mark to facebook_problems.py

--- facebook_problems.py
+++ facebook_problems.py
@@ -15,6 +15,16 @@ def reverse32(x)
     
 # Driver code
  x = 100
+ 
+ 
+ #2 In chess, the Elo rating system is used to calculate player strengths based on game results.
+#
+# A simplified description of the Elo system is as follows. Every player begins at the same score. For each subsequent
+# game, the loser transfers some points to the winner, where the amount of points transferred depends on how unlikely
+# the win is. For example, a 1200-ranked player should gain much more points for beating a 2000-ranked player than
+# for beating a 1300-ranked player.
+#
+# Implement this system.
 
 
 
