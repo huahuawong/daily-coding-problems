751204586b29e1720a58aaa6c0f4ddb424207b5c
huahuawong <junhuawong96@gmail.com>
2020-05-27 23:50:35 -0400

    Create microsoft_problems.py

---
+++ microsoft_problems.py
@@ -0,0 +1,3 @@
+# Q1 Write a program to determine how many distinct ways there are to create a max heap from a list of N given integers.
+
+# For example, if N = 3, and our integers are [1, 2, 3], there are two ways, shown below.
