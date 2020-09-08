322b99f9702cdfa6b11befd26f184d702a9991ff
Jun Hua Wong <junhuawong96@gmail.com>
2020-09-07 22:34:55 -0500

    Create C3_stacks&queue.py

---
+++ CTCI/C3_stacks&queue.py
@@ -0,0 +1,5 @@
+# 3.1 Describe how you could use a single array to implement three stacks
+
+# First, initialise 2 stacks by using the starting elements and ending elements of the array
+# Second, define the third stack at the middle of the array
+# Redefine push op s.t. when a new element is pushed into a stack, the other stacks will be shifted
