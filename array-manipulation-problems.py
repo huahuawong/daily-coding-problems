5105d708d7f0ef012ff512c20d2b86652c8efc32
Jun Hua Wong <junhuawong96@gmail.com>
2020-09-13 23:59:10 -0400

    Update array-manipulation-problems.py

--- array-manipulation-problems.py
+++ array-manipulation-problems.py
@@ -275,3 +275,29 @@ def findAnagrams(s, p):
 
 print(find_anagram(s, word))
 
+###########################################################################################################################################
+# 11. Integer to Roman conversion
+def int_to_Roman(num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0 
+        while num > 0:
+            for iteration in range(num// val[i]):
+                roman_num += syb[i]
+                num += val[i]
+            i -= 1
+        return roman_num             
+
+print(int_to_Roman(45))
+
