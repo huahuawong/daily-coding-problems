469e49fd3abe5b179f8077ef8b9d79b5dbeaec65
Jun Hua Wong <junhuawong96@gmail.com>
2020-11-21 16:32:29 -0500

    Update and rename google-problems to google-problems.py

--- google-problems.py
+++ google-problems.py
@@ -124,6 +124,8 @@ else:
     print("Matrix is not a Toeplitz")
 
  
+ #4 You are given an array of nonnegative integers. Let's say you start at the beginning of the array and are trying to advance to the end. You can advance
+ # at most, the number of steps that you're currently on. Determine whether you can get to the end of the array.
  
  
      
