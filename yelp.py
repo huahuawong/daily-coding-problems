0d6645e0510fa8ec31500a5c5d88b05f5361eb00
Jun Hua Wong <junhuawong96@gmail.com>
2020-11-20 20:57:24 -0500

    Create yelp.py

---
+++ yelp.py
@@ -0,0 +1,23 @@
+def find_min_jump(arr, n):
+    jumps = [0 for i in range(n)]
+
+    # Eliminate some base case
+    if (n == 0) or (arr[0] == 0):
+        return float('inf')
+
+    jumps[0] = 0
+
+    for i in range(1, n):
+        jumps[i] = float('inf')
+        for j in range(i):
+            if i <= (arr[j] + j) and (jumps[j] != float('inf')):
+                jumps[i] = min(jumps[i], jumps[j] + 1)
+                break
+    return jumps[n - 1]
+
+
+# Driver Program to test above function
+arr = [1, 3, 6, 1, 0, 9]
+size = len(arr)
+print('Minimum number of jumps to reach',
+      'end is', find_min_jump(arr, size))
