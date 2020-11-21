6e10262665156d80ef2455c7cd808c0fff1a3d8f
Jun Hua Wong <junhuawong96@gmail.com>
2020-11-20 20:58:18 -0500

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -1,3 +1,7 @@
+# Q1
+# You are given an array of integers, where each element represents the maximum number of steps that can be jumped going forward from that element. 
+# Write a function to return the minimum number of jumps you must take in order to get from the start to the end of the array.
+
 def find_min_jump(arr, n):
     jumps = [0 for i in range(n)]
 
