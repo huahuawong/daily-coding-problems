6293245a2f8424c59f6a477d5714e0ee444bd7c1
Jun Hua Wong <junhuawong96@gmail.com>
2021-01-07 23:48:45 -0500

    Update google-problems.py

--- google-problems.py
+++ google-problems.py
@@ -198,6 +198,10 @@ def find_smallest(arr, n):
 arr = [5, 7, 3, 8, 1]
 print(find_smallest(arr, len(arr)))
 
+# 7 This problem was asked by Google.
+# Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list.
+
+
 
 
 
