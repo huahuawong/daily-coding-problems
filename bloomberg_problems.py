6ecbbb1f96434931d8da7d1d234604e9c500343a
Jun Hua Wong <junhuawong96@gmail.com>
2021-01-27 22:09:57 -0500

    Create bloomberg_problems.py

---
+++ bloomberg_problems.py
@@ -0,0 +1,14 @@
+def isIsomorphic(s, t):
+    s2t, t2s = {}, {}
+    for i in range(len(s)):
+        if s[i] in s2t and s2t[s[i]] != t[i]:
+            return False
+        if t[i] in t2s and t2s[t[i]] != s[i]:
+            return False
+        s2t[s[i]] = t[i]
+        t2s[t[i]] = s[i]
+    return True
+
+
+str1 = "aab"; str2 = "xyy"
+print(isIsomorphic(str1, str2))
