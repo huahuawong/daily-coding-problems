5535afbc0023a802cbd9e822e16dcfbf28d86419
Jun Hua Wong <junhuawong96@gmail.com>
2021-01-29 14:01:33 -0500

    Update bloomberg_problems.py

--- bloomberg_problems.py
+++ bloomberg_problems.py
@@ -1,5 +1,7 @@
 # Q1 Determine whether there exists a one-to-one character mapping from one string s1 to another s2
-
+# We need to first understand the question, we want to find a one-to-one relationship, does not necessarily have to be in alphabetical order.
+# To solve this problem, essentially we can initialize two empty dict, and append elements from the other array if the element is not in the dict and the value of the key 
+# has the same value. Returns False if the element is present in the dictionary AND the value of the key is different from the element at index i
 def isIsomorphic(s, t):
     s2t, t2s = {}, {}
     for i in range(len(s)):
