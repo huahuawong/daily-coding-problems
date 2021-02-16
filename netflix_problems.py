915a9c0ea3e6189f688464e32d3a557ac67bb031
Jun Hua Wong <junhuawong96@gmail.com>
2021-02-15 22:16:34 -0500

    Update netflix_problems.py

--- netflix_problems.py
+++ netflix_problems.py
@@ -1,1 +1,8 @@
-# 1 Given an array of integers, determine whether it contains a Pythagorean triplet. Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c2.
+# 1 Given an array of integers, determine whether it contains a Pythagorean triplet. Recall that a Pythogorean triplet (a, b, c) is defined by the
+# equation a^2+ b^2= c^2.
+
+def find_pythagoras(num1, num2, num3):
+    return (num1 ** 2) + (num2 ** 2) == (num3 ** 2)
+    
+print(find_pythagoras(3, 4, 5))
+
