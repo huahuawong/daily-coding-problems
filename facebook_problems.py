46a3e06cd343f9e44a3a82fdc8d0d736081f7605
Jun Hua Wong <junhuawong96@gmail.com>
2021-09-23 23:49:59 -0400

    Update facebook_problems.py

--- facebook_problems.py
+++ facebook_problems.py
@@ -139,3 +139,11 @@ find_max_prod(arr)
 def find_max_prod(arr):
     arr.sort()    
     return max((arr[0] * arr[1] * arr[-1]), arr[-1] * arr[-2] * arr[-3])
+
+
+# 6 Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
+# For example, given the string "([])[]({})", you should return true.
+# Given the string "([)]" or "((()", you should return false.
+
+
+
