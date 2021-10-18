7e356d401ca4dbac7d5e3dbe4e5db4f1b5e34aba
Jun Hua Wong <junhuawong96@gmail.com>
2021-10-17 22:42:33 -0400

    Update google_problems.py

--- google_problems.py
+++ google_problems.py
@@ -263,3 +263,12 @@ def estimate_pi(INTERVAL):
 
 # print(rand_x, rand_y, circle_points, square_points, "-", pi)
 print("Final Estimation of Pi=", estimate_pi(5))
+
+
+# 10. Given an array of elements, return the length of the longest subarray where all its elements are distinct.
+# For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
+def find_longest(num):
+       return (len(set(num)))
+
+arr = [5, 1, 3, 5, 2, 3, 4, 1]
+print(find_longest(arr))
