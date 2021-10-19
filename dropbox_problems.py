1315119aebda5dfd63b9ff5e12856c34f5779415
Jun Hua Wong <junhuawong96@gmail.com>
2021-10-18 23:41:03 -0400

    Update dropbox_problems.py

--- dropbox_problems.py
+++ dropbox_problems.py
@@ -20,3 +20,37 @@ def num_hash(n):
     
 n = 702
 print(num_hash(n))
+
+# 2 Given a string s and a list of words words, where each word is the same length, find all starting indices of
+# substrings in s that is a concatenation of every word in words exactly once.
+#
+# For example, given s = "dogcatcatcodecatdog" and words = ["cat", "dog"], return [0, 13], since "dogcat" starts at
+# index 0 and "catdog" starts at index 13.
+#
+# Given s = "barfoobazbitbyte" and words = ["dog", "cat"], return [] since there are no substrings composed of "dog"
+# and "cat" in s.
+
+import itertools
+
+words = ["cat", "dog"]
+s = "dogcatcatcodecatdog"
+# s = "barfoobazbitbyte"
+
+def find_starting_index(s, words):
+    possible_words = list(itertools.permutations(words))
+
+    possible_concats = []
+
+    for word in possible_words:
+        possible_concats.append(''.join(word))
+
+    index_arr = []
+    for word in possible_concats:
+        index = s.find(word)
+        if index == -1:
+            return []
+        index_arr.append(index)
+    return index_arr
+
+
+print(find_starting_index(s, words))
