242f105d565f4f75888b96f0527ef83e492af54d
Jun Hua Wong <junhuawong96@gmail.com>
2021-10-24 23:20:03 -0500

    Create paypal.py

---
+++ paypal.py
@@ -0,0 +1,2 @@
+# Q1 Given a string and a number of lines k, print the string in zigzag form. In zigzag, characters are printed out diagonally from top left to bottom right until
+# reaching the kth line, then back up to top right, and so on.
