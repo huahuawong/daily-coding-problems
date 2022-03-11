f5cb9da8b3371f58205f3e31f27d7e9f0463ec1d
Jun Hua Wong <junhuawong96@gmail.com>
2022-03-10 23:27:02 -0500

    Update google_problems.py

--- google_problems.py
+++ google_problems.py
@@ -273,3 +273,10 @@ def find_longest(num):
 arr = [5, 1, 3, 5, 2, 3, 4, 1]
 print(find_longest(arr))
 
+
+# 11. Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
+# For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
+
+# In this example, assume nodes with the same value are the exact same node objects.
+# Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+
