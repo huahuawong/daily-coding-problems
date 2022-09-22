b801d455db081b9e6702673b1ec952e91b771b54
Jun Hua Wong <junhuawong96@gmail.com>
2022-09-21 23:31:44 -0400

    Create 2. longest-substring-without-repeating-characters.py

---
+++ blind75/2. longest-substring-without-repeating-characters.py
@@ -0,0 +1,9 @@
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
