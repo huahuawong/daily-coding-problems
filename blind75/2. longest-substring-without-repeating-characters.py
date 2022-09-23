16e71e8fb79f94b2ee1f4a8fb3d6cec3aa9db568
Jun Hua Wong <junhuawong96@gmail.com>
2022-09-22 23:15:29 -0400

    Update 2. longest-substring-without-repeating-characters.py

--- blind75/2. longest-substring-without-repeating-characters.py
+++ blind75/2. longest-substring-without-repeating-characters.py
@@ -1,3 +1,5 @@
+# Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
 # Input: s = "abcabcbb"
 # Output: 3
 # Explanation: The answer is "abc", with the length of 3.
@@ -7,3 +9,37 @@
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+# 'Sliding window' way
+def find_longest_substring(s):
+    window = []; max_len = 0
+    for i in range(0, len(s)):
+        if not s[i] in window:
+            window.append(s[i])
+            continue
+        if s[i] in window:
+            if window[0] == s[i]:
+                max_len = max(max_len, len(window))
+                window.pop(0)
+                window.append(s[i])
+                continue
+            else:
+                max_len = max(max_len, len(window))
+                window = []
+                window.append(s[i])
+                continue
+    return max_len
+  
+  
+  
+# Alernative way
+def find_longest_substring(s):
+    start = max_len = 0
+    string_dict = {}
+    for i in range(len(s)):
+        if s[i] in string_dict and start <= string_dict[s[i]]:
+            start = string_dict[s[i]] + 1           
+        else:
+            max_len = max(max_len, i - start + 1)
+        string_dict[s[i]] = i
+    return max_len
+               
