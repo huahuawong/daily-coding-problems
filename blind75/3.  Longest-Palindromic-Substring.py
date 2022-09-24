cac0468c70203d7fbb74b76b8659f88a9b0cb98c
Jun Hua Wong <junhuawong96@gmail.com>
2022-09-23 22:00:51 -0400

    Create 3.  Longest-Palindromic-Substring.py

---
+++ blind75/3.  Longest-Palindromic-Substring.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+  
+# Given a string s, return the longest palindromic substring in s.
+# A string is called a palindrome string if the reverse of that string is the same as the original string.
+
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+
+# Input: s = "cbbd"
+# Output: "bb"
+
