4bf8304755dbe8d96a502204e8a37da73609f135
Jun Hua Wong <junhuawong96@gmail.com>
2022-09-26 23:57:14 -0400

    Update 3.  Longest-Palindromic-Substring.py

--- blind75/3.  Longest-Palindromic-Substring.py
+++ blind75/3.  Longest-Palindromic-Substring.py
@@ -10,3 +10,39 @@
 # Input: s = "cbbd"
 # Output: "bb"
 
+
+# The following code works but it is not memory efficient, so let's try a second option
+def longestPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    n = len(s)
+    max_len = n
+    k = n
+
+    if len(s) == 1:
+        return s
+
+    while n!= 1:
+        if k == n:
+            window = s[0:n]
+            if window == window[::-1]:
+                return window
+            else:
+                k -= 1
+                continue
+
+        for i in range(k):
+            window = s[0:k]
+            if window == window[::-1]:
+                return window
+
+        for i in range(max_len-k):
+            window = window[1:] + s[k+i]
+            if window == window[::-1]:
+                return window
+            else:
+                continue
+        k -= 1
+        n -= 1
