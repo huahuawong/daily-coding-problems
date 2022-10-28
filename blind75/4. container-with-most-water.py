cbd4bb47d95eadab511ffa683d41115a4c96b8fc
Jun Hua Wong <junhuawong96@gmail.com>
2022-10-28 00:17:00 -0400

    Update 4. container-with-most-water.py

--- blind75/4. container-with-most-water.py
+++ blind75/4. container-with-most-water.py
@@ -1,3 +1,16 @@
 # Full description on the question: https://leetcode.com/problems/container-with-most-water/
 
 
+def maxArea(self, height: List[int]) -> int:
+    ans = 0
+    i = 0; j = len(height) - 1
+
+    while (i < j):
+        if height[i] < height[j]:
+            res = height[i] * (j-i)
+            i += 1
+        else:
+            res = height[j] * (j-i)
+            j -= 1
+        ans = max(res, ans)
+    return ans
