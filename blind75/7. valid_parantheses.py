5aaa10a446c9bb7f6f94908454323d4f2134ada1
Jun Hua Wong <junhuawong96@gmail.com>
2022-11-10 23:57:44 -0500

    Create 7. valid_parantheses.py

---
+++ blind75/7. valid_parantheses.py
@@ -0,0 +1,4 @@
+# Link to the problem: https://leetcode.com/problems/valid-parentheses/
+
+# A quick and easy way is to initialize two list, one for "left", one for "right"
+# And as we go through the string, we can check if its in the left or right, at the end of the day if it is not valid, then we wouldnt have an empty string, if so, return False
