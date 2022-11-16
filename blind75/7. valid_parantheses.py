96d9646a4a4864600570f84bd68da8dca4418e47
Jun Hua Wong <junhuawong96@gmail.com>
2022-11-15 23:41:39 -0500

    Update 7. valid_parantheses.py

--- blind75/7. valid_parantheses.py
+++ blind75/7. valid_parantheses.py
@@ -2,3 +2,32 @@
 
 # A quick and easy way is to initialize two list, one for "left", one for "right"
 # And as we go through the string, we can check if its in the left or right, at the end of the day if it is not valid, then we wouldnt have an empty string, if so, return False
+
+
+def isValid(self, s: str) -> bool:
+    left =  ['(', '[', '{']
+    right = [')', ']', '}']
+
+    if len(s) % 2 != 0:
+        return False
+
+    if s[0] in right:
+        return False
+
+    check_arr = []
+
+    for character in s:
+        if character in left:
+            check_arr.append(left.index(character))
+        else:
+            if len(check_arr) == 0:
+                # print("False")
+                return False
+            right_index = right.index(character)
+
+            if check_arr[-1] == right_index:
+                check_arr.pop()
+            else:
+                return False
+
+    return (len(check_arr) == 0)
